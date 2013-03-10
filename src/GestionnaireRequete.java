df81fb78d9901d9ed1433a2a597e7a3de392d111
Cindy Perat <cindy.perat@live.fr>
2013-03-10 23:14:23 +0100

    Récupération du type de requête pas encore fonctionnel

--- src/GestionnaireRequete.java
+++ src/GestionnaireRequete.java
@@ -1,66 +1,26 @@
 import java.util.StringTokenizer;
 
 public class GestionnaireRequete {
-
-	private String reponse;
-	private String erreur;
-	private String[] operators = { "+", "-", "*", "/" };
-
+	public String erreur;
+	public String reponse;
+	private String[] typesRequetes = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"};
+	
 	public void traitementRequete(String requete) {
-		erreur = "";
 		reponse = "";
-		int op1;
-		int op2;
-
+		
 		if (requete.toUpperCase().equals("QUITTER")) {
 			reponse = "Ok. Le serveur va s'éteindre";
 			return;
 		}
-
-		for (String operator : operators) {
-			StringTokenizer st = new StringTokenizer(requete, operator);
-			if (st.countTokens() == 2) {
-				try {
-					op1 = Integer.parseInt(st.nextToken());
-					op2 = Integer.parseInt(st.nextToken());
-					reponse = "" + evaluate(operator.charAt(0), op1, op2);
-				} catch (NumberFormatException nfe) {
-					erreur = "Mauvais format de donnee";
-				}
-				break;
-			}
-		}
-		if (reponse.equals("") && erreur.equals(""))
-			erreur = "Erreur de syntaxe dans la requete";
+		
+		StringTokenizer st = new StringTokenizer(requete, "#");
+		reponse = st.nextToken();
 	}
-
+	
 	public String reponse() {
 		if (erreur.equals(""))
 			return reponse;
 		return erreur;
 	}
-
-	private int evaluate(char operator, int operande1, int operande2) {
-		int resultat = 0;
-		switch (operator) {
-		case '+':
-			resultat = operande1 + operande2;
-			break;
-		case '-':
-			resultat = operande1 - operande2;
-			break;
-		case '*':
-			resultat = operande1 * operande2;
-			break;
-		case '/':
-			if (operande2 == 0)
-				erreur = "Division par 0";
-			else
-				resultat = operande1 / operande2;
-			break;
-		}
-		return resultat;
-	}
-
 }
 
