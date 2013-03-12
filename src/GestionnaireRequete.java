fb147fd249a4063022572336504f0b3c8ceb330a
Cindy Perat <cindy.perat@live.fr>
2013-03-12 22:39:25 +0100

    Traduction du message transmit au serveur qui mène à une fonction

--- src/GestionnaireRequete.java
+++ src/GestionnaireRequete.java
@@ -16,13 +16,91 @@ public class GestionnaireRequete {
 		
 		StringTokenizer st = new StringTokenizer(requete, "#");
 		if (st.countTokens() == 2) {
-			String tmp = st.nextToken();
+			String lettreRequete = st.nextToken();
+			String infosRequete = st.nextToken();
 			for (String typeRequete : typesRequetes) {
-				if (tmp.equals(typeRequete)) {
-					reponse = tmp;
-				}
-				else {
-					reponse = "Requête demandée incorrecte.";
+				if (lettreRequete.equals(typeRequete)) {
+					if (typeRequete.equals("A")) {
+						StringTokenizer st2 = new StringTokenizer(infosRequete, ",");
+						if (st2.countTokens() == 3) {
+							int code = Integer.parseInt(st2.nextToken());
+							String nom = st2.nextToken();
+							int duree = Integer.parseInt(st2.nextToken());
+							ajouterTache(code, nom, duree);
+						}
+					}
+					else if (typeRequete.equals("B")) {
+						StringTokenizer st2 = new StringTokenizer(infosRequete, ",");
+						if (st2.countTokens() == 3) {
+							int code = Integer.parseInt(st2.nextToken());
+							String nom = st2.nextToken();
+							int duree = Integer.parseInt(st2.nextToken());
+							modifierTache(code, nom, duree);
+						}
+					}
+					else if (typeRequete.equals("C")) {
+						if (infosRequete.length() == 1) {
+							int code = Integer.parseInt(infosRequete);
+							supprimerTache(code);
+						}
+					}
+					else if (typeRequete.equals("D")) {
+						StringTokenizer st2 = new StringTokenizer(infosRequete, ",");
+						if (st2.countTokens() == 2) {
+							int code = Integer.parseInt(st2.nextToken());
+							String nom = st2.nextToken();
+							ajouterPersonne(code, nom);
+						}
+					}
+					else if (typeRequete.equals("E")) {
+						StringTokenizer st2 = new StringTokenizer(infosRequete, ",");
+						if (st2.countTokens() == 2) {
+							int code = Integer.parseInt(st2.nextToken());
+							String nom = st2.nextToken();
+							modifierPersonne(code, nom);
+						}
+					}
+					else if (typeRequete.equals("F")) {
+						StringTokenizer st2 = new StringTokenizer(infosRequete, ",");
+						if (st2.countTokens() == 2) {
+							int code = Integer.parseInt(st2.nextToken());
+							String nom = st2.nextToken();
+							supprimerPersonne(code, nom);
+						}
+					}
+					else if (typeRequete.equals("G")) {
+						StringTokenizer st2 = new StringTokenizer(infosRequete, ",");
+						if (st2.countTokens() == 2) {
+							int codeTache = Integer.parseInt(st2.nextToken());
+							int codePersonne = Integer.parseInt(st2.nextToken());
+							ajouterAffectation(codeTache, codePersonne);
+						}
+					}
+					else if (typeRequete.equals("I")) {
+						StringTokenizer st2 = new StringTokenizer(infosRequete, ",");
+						if (st2.countTokens() == 2) {
+							int codeTache = Integer.parseInt(st2.nextToken());
+							int codePersonne = Integer.parseInt(st2.nextToken());
+							modifierAffectation(codeTache, codePersonne);
+						}
+					}
+					else if (typeRequete.equals("J")) {
+						StringTokenizer st2 = new StringTokenizer(infosRequete, ",");
+						if (st2.countTokens() == 2) {
+							int codeTache = Integer.parseInt(st2.nextToken());
+							int codePersonne = Integer.parseInt(st2.nextToken());
+							supprimerAffectation(codeTache, codePersonne);
+						}
+					}
+					else if (typeRequete.equals("K")) {
+						
+					}
+					else if (typeRequete.equals("L")) {
+						
+					}
+					else if (typeRequete.equals("M")) {
+						
+					}
 				}
 			}
 		}
@@ -37,6 +115,61 @@ public class GestionnaireRequete {
 			return reponse;
 		}
 		return erreur;
+	}
+	
+	public void ajouterTache(int code, String nom, int duree) {
+		reponse = "Ajouter tâche : " + code + ", " + nom + ", " + duree;
+	}
+	
+	public void modifierTache(int code, String nom, int duree) {
+		reponse = "Modifier tâche : " + code + ", " + nom + ", " + duree;
+	}
+	
+	public void supprimerTache(int code) {
+		reponse = "Supprimer tâche : " + code;
+	}
+	
+	public void ajouterPersonne(int code, String nom) {
+		reponse = "Ajouter personne : " + code + ", " + nom;
+	}
+	
+	public void modifierPersonne(int code, String nom) {
+		reponse = "Modifier personne : " + code + ", " + nom;
+	}
+	
+	public void supprimerPersonne(int code, String nom) {
+		reponse = "Supprimer personne : " + code + ", " + nom;
+	}
+	
+	public void ajouterAffectation(int codeTache, int codePersonne) {
+		reponse = "Ajouter affectation : " + codeTache + ", " + codePersonne;
+	}
+	
+	public void modifierAffectation(int codeTache, int codePersonne) {
+		reponse = "Modifier affectation : " + codeTache + ", " + codePersonne;
+	}
+	
+	public void supprimerAffectation(int codeTache, int codePersonne) {
+		reponse = "Supprimer affectation : " + codeTache + ", " + codePersonne;
+	}
+	
+	public void ajouterContrainte(int codePredecesseur, int codeSuccesseur, int duree, int type) {
+		reponse = "Ajouter contrainte : " + codePredecesseur + ", " + codeSuccesseur + ", " +
+					duree + ", " + type;
+	}
+	
+	public void modifierContrainte(int codePredecesseur, int codeSuccesseur, int duree, int type) {
+		reponse = "Modifier contrainte : " + codePredecesseur + ", " + codeSuccesseur + ", " +
+					duree + ", " + type;
+	}
+	
+	public void supprimerContrainte(int codePredecesseur, int codeSuccesseur, int duree, int type) {
+		reponse = "Supprimer contrainte : " + codePredecesseur + ", " + codeSuccesseur + ", " +
+					duree + ", " + type;
+	}
+	
+	public void chargerProjet(int codeProjet) {
+		reponse = "Chargement projet : " + codeProjet;
 	}
 }
 
