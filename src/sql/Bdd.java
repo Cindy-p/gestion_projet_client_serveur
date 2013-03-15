0fdb33b13a0aaebf720078077a8ea22fd3d478bd
Cindy Perat <cindy.perat@live.fr>
2013-03-15 11:07:14 +0100

    mise à jour des fichiers

--- src/sql/Bdd.java
+++ src/sql/Bdd.java
@@ -10,79 +10,84 @@ public class Bdd {
 	private String password;
 	private String user;
 	private Connection conn;
-	
-	public Bdd (){
+
+	public Bdd() {
 		String pilote = "com.mysql.jdbc.Driver";
-		
+
 		try {
 			Class.forName(pilote);
-//			System.out.println("Chargement driver OK.");
-			
+			// System.out.println("Chargement driver OK.");
+
 			String url = "jdbc:mysql://localhost/gestion_proj";
 			user = "root";
 			password = "";
-			
+
 			conn = DriverManager.getConnection(url, user, password);
-//			System.out.println("Connexion effective.");
-			
+			// System.out.println("Connexion effective.");
+
 		} catch (Exception ex) {
 			System.err.println("Erreur : " + ex);
 		}
 	}
-	
+
 	public boolean executerRequeteSelection(String sql) {
 		boolean rep = true;
-		int num_projet = 0;	
+		int num_projet = 0;
+		int taille = 0;
 		System.out.println("\tRequête à executer : " + sql);
-		
+
 		try {
 			Statement ordre = conn.createStatement();
 			ResultSet resultat = ordre.executeQuery(sql);
-			
-			
+
 			int nbCols = resultat.getMetaData().getColumnCount();
-			
+
 			// Pour chaque ligne
 			while (resultat.next()) {
 				// Pour chaque colonne
-				for (int col = 1 ; col <= nbCols ; col++) {
+				for (int col = 1; col <= nbCols; col++) {
 					String type = resultat.getMetaData().getColumnTypeName(col);
 					if (type.equals("VARCHAR"))
-						System.out.println("\tDescription : " + resultat.getString(col));
-					else if (type.equals("INT UNSIGNED")){
+						System.out.println("\tDescription : "
+								+ resultat.getString(col));
+					else if (type.equals("INT UNSIGNED")) {
 						num_projet = resultat.getInt(col);
 						System.out.println("\n\tN° projet : " + num_projet);
-					}
-					else if (type.equals("CHAR"))
-						System.out.println("\tNom : " + resultat.getString(col));
-				} 
-			} 
+					} else if (type.equals("CHAR"))
+						System.out
+								.println("\tNom : " + resultat.getString(col));
+				}
+				taille++;
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			System.err.println("Erreur : " + ex);
 			rep = false;
 		}
+		if (taille == 0)
+			rep = false;
+
 		System.out.println("\n\tRequête effectuée : " + rep);
-		if (rep == true && num_projet != 0){
+		if (rep == true && num_projet != 0) {
 			System.out.println("\n\tChangement de projet...");
 			System.out.println("\nOpérations sur le projet n°" + num_projet);
 		}
 		return rep;
 	}
-	
+
 	public boolean executerRequeteMAJ(String req) {
 		int res = -1;
 		boolean rep = true;
-		
+
 		System.out.println("\tRequête à executer : " + req);
-		
+
 		try {
 			Statement ordre = conn.createStatement();
 			res = ordre.executeUpdate(req);
-			
-			if(res == -1 || res == 0)
+
+			if (res == -1 || res == 0)
 				rep = false;
-			
+
 		} catch (Exception ex) {
 			System.err.println("Erreur : " + ex);
 			rep = false;
